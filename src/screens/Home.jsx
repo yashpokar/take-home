e454d63e888c71596acc930345174a50c36132d5
yashpokar <yash.p@digitalshelfiq.com>
2019-01-16 13:08:57 +0530

    frame is ready

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
 	render () {
 		return (
-			<div className="">
-				Home Screen from React Component
+			<div className="container">
+				<nav className="navigation">
+					<Link className="navigation__title" to='/'>IMPEKABLE</Link>
+				</nav>
+
+				<aside className="sidebar">
+					aside
+				</aside>
 			</div>
 		)
 	}
